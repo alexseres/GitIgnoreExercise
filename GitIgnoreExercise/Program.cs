817eca4e465cf8005e24f0033873364e58379402
alexseres <lilaalex95@gmail.com>
2020-09-03 05:52:10 -0700

    Add project files.

---
+++ GitIgnoreExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitIgnoreExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
